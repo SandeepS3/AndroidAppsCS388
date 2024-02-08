0cfb42cb66f38ff9b974b6650622e6287031b618
Sandeep Singh <singh.deep8302@gmail.com>
2024-02-08 15:26:33 -0500

    lab3

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        
+
         // Lookup the RecyclerView in activity layout
         val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
         // Fetch the list of emails
